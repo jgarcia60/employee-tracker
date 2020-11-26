89703bb2705f79928ab0c0a162b9f7de06ec80bc
jgarcia60 <68722324+jgarcia60@users.noreply.github.com>
2020-11-25 20:32:25 -0500

    added view by manager, update manager,and optimized add employee

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const questions = [
         type: 'list',
         message: 'What would you like to do?',
         name: 'option',
-        choices: ['Add department, role, or employee', 'View department, role, or employee', 'Update employee role', 'Update employee manager', 'View employees by manager', 'Delete department, role, or employee', 'View total utilized budget of a department', 'Exit']
+        choices: ['Add employee', 'View department, role, or employee', 'Update employee role', 'Update employee manager', 'View employees by manager', 'Delete department, role, or employee', 'View total utilized budget of a department', 'Exit']
     }
 ]
 
@@ -38,7 +38,7 @@ init = () => {
     inquirer.prompt(questions).then((res) => {
         //if you want to add an employee
         switch(res.option) {
-            case "Add department, role, or employee":
+            case "Add employee":
                 // const testObj = {first_name: "Annika", last_name: "Garcia", role_id: 70, manager_id: 2}
                 add();
                 // queryAdd(testObj);
@@ -82,12 +82,41 @@ init = () => {
     });
 };
 
-const tableType = [
+const newEmployeeQs = [
     {
-        type: 'list',
-        message: 'Which object are you wanting to add?',
-        name: 'table',
-        choices: ['employee', 'role', 'department']
+        type: 'input',
+        message: "What is the employee's first name?",
+        name: 'first'
+    },
+    {
+        type: 'input',
+        message: "What is the employee's last name?",
+        name: 'last'
+    },
+    {
+        type: 'input',
+        message: "What is the employee's role ID?",
+        name: 'roleId'
+    },
+    {
+        type: 'input',
+        message: "What is the employee's manager's ID?",
+        name: 'managerId'
+    },
+    {
+        type: 'input',
+        message: "What is the employee's title?",
+        name : 'title'
+    },
+    {
+        type: 'input',
+        message: "What is the employee's salary?",
+        name: 'salary'
+    },
+    {
+        type: 'input',
+        message: "What is the employee's department?",
+        name: 'department'
     }
 ]
 
@@ -141,46 +170,75 @@ const deptQs = [
         name: 'department'
     }
 ];
-queryAdd = (type, object) => {
-    connection.query("INSERT INTO " + type + " SET ?", object, (err, res) => { //might be outside scope of the connection, cannot see database
+queryAdd = (employee, role, department) => {
+    connection.query("INSERT INTO employee SET ?", employee, (err, res) => { //might be outside scope of the connection, cannot see database
+        if (err) throw err;
+        console.log(res.affectedRows + "successfully added");
+        // init();
+    });
+    connection.query("INSERT INTO role SET ?", role, (err, res) => { //might be outside scope of the connection, cannot see database
+        if (err) throw err;
+        console.log(res.affectedRows + "successfully added");
+        // init();
+    });
+    connection.query("INSERT INTO department SET ?", department, (err, res) => { //might be outside scope of the connection, cannot see database
         if (err) throw err;
         console.log(res.affectedRows + "successfully added");
         init();
     });
-}
+    
+};
 add = () => {
-    inquirer.prompt(tableType).then((res) => { //add specific questions for each if block
+    inquirer.prompt(newEmployeeQs).then((res) => { //add specific questions for each if block
         // let csv;
-        const query = res.table;
-        if (res.table === 'employee') {
-            inquirer.prompt(employeeQs).then((res) => {
-                const newEmployee = {
-                    first_name: res.first,
-                    last_name: res.last,
-                    role_id: res.roleId,
-                    manager_id: res.managerId
-                }
-                queryAdd(query, newEmployee);
-                // const arr = [res.first, res.last, res.roleId, res.managerId];
+        // const query = res.table;
+        const newEmployee = {
+            first_name: res.first,
+            last_name: res.last,
+            role_id: res.roleId,
+            manager_id: res.managerId
+        };
+        const newRole = {
+            id: res.roleId,
+            title: res.title,
+            salary: res.salary,
+            department_id: res.department
+        };
+        const newDept = {
+            name: res.department
+        };
+        queryAdd(newEmployee, newRole, newDept);
+        // queryAdd("role", newRole);
+        // queryAdd("department", newDept);
+        // if (res.table === 'employee') {
+        //     inquirer.prompt(employeeQs).then((res) => {
+        //         const newEmployee = {
+        //             first_name: res.first,
+        //             last_name: res.last,
+        //             role_id: res.roleId,
+        //             manager_id: res.managerId
+        //         }
+        //         queryAdd(query, newEmployee);
+        //         // const arr = [res.first, res.last, res.roleId, res.managerId];
                 
-            });
-        } else if (res.table === 'role') {
-            inquirer.prompt(roleQs).then((res) => {
-                const newRole = {
-                    title: res.title,
-                    salary: res.salary,
-                    department_id: res.department
-                };
-                queryAdd(query, newRole);
-            });
-        } else if (res.table === 'department') {
-            inquirer.prompt(deptQs).then((res) => {
-                const newDept = {
-                    name: res.department
-                };
-                queryAdd(query, newDept);
-            })
-        }
+        //     });
+        // } else if (res.table === 'role') {
+        //     inquirer.prompt(roleQs).then((res) => {
+        //         const newRole = {
+        //             title: res.title,
+        //             salary: res.salary,
+        //             department_id: res.department
+        //         };
+        //         queryAdd(query, newRole);
+        //     });
+        // } else if (res.table === 'department') {
+        //     inquirer.prompt(deptQs).then((res) => {
+        //         const newDept = {
+        //             name: res.department
+        //         };
+        //         queryAdd(query, newDept);
+        //     })
+        // }
     });
 };
 
@@ -277,21 +335,54 @@ updateManager = (input) => {
     })
 
 }
+const employeeByManagerQs = [
+    {
+        type: 'input',
+        message: 'Enter the manager ID for the employees you want to view:',
+        name: 'manager_id'
+    }
+]
 
-employeesByManager = () => {
-
-    connection.query("SELECT * FROM ?", [input], (err, res) => { //change query call
+queryByManager = (manager_id) => {
+    connection.query("SELECT * FROM employee JOIN role ON employee.role_id = role.id WHERE manager_id = ?;", [manager_id], (err, res) => { 
         if (err) throw err;
         console.table(res);
         init();
         // connection.end();
-    })
+    });
+};
+employeesByManager = () => {
+    inquirer.prompt(employeeByManagerQs).then((res) => {
+        const manager_id = parseInt(res.manager_id);
+        queryByManager(manager_id);
+    });
+};
 
-}
+const deleteQs = [
+    {
+        type: 'input',
+        message: "Whose info would you like to delete?",
+        name: 'name'
+    }
+];
 
+queryDelete = (first, last) => {
+    connection.query("DELETE * FROM employee WHERE first_name = ? AND last_name = ?", [first, last], (err, res) => {
+        if (err) throw err;
+        console.log(res.affectedRows + " was deleted!");
+    });
+    init();
+};
 deleteEntry = () => {
-
-    connection.query("SELECT * FROM ?", [input], (err, res) => { //change query call
+    inquirer.prompt(deleteQs).then((res) => {
+        const name = res.name;
+       
+        let temp = name.split(" ");
+        const first = temp[0];
+        const last = temp[1];
+        queryDelete(first, last);
+    })
+    connection.query("DELETE * FROM ?", [input], (err, res) => { //change query call
         if (err) throw err;
         console.table(res);
         init();
